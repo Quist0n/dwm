638c0987db9aba3c1d2fe030260a8c7511166fd2
Quist0n <37319086+Quist0n@users.noreply.github.com>
2022-01-30 20:21:28 -0500

    Make the dmenu_run spawn on the bottom of the screen

--- config.h
+++ config.h
@@ -91,7 +91,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_color1, "-nf", col_color3, "-sb", col_custom, "-sf", col_color4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_color1, "-nf", col_color3, "-sb", col_custom, "-sf", col_color4, "-b", NULL };
 //static const char *termcmd[]  = { "alacritty", NULL };
 
 

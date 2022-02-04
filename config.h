eb460597ed152b2eaf11644a774eaf5dc2be59c4
Quist0n <37319086+Quist0n@users.noreply.github.com>
2022-02-04 00:03:05 -0500

    Add alt binding for opening apps

--- config.h
+++ config.h
@@ -95,6 +95,7 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_r,      spawn,          {.v = dmenucmd } },
 //	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
